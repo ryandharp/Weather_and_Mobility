de8353a4cc56b45caa923d4cc9025635578963c5
Ryan Harp <ryan.harp@northwestern.edu>
2021-03-02 08:24:40 -0700

    more efficient version of "true visit" factor aggregator

--- scripts/mobility_data_aggregator.py
+++ scripts/mobility_data_aggregator.py
@@ -2,7 +2,7 @@
 This script will produce "true" visit totals by day for all POIs of a specified
 NAICS code for one month.
 
-Created 2/4/2021 by Ryan Harp.
+Created 2/23/2021 by Ryan Harp.
 """
 
 
@@ -16,12 +16,15 @@ fname = '2018-01'
 date_start = "2018-01-01"
 date_end = "2018-01-31"
 
-# limiting data for processing
-df_mobility = pd.read_csv('/projects/b1045/Safegraph_v2/mobility_data/monthly_combined_files/'+fname+'_compiled.csv')
-#df_mobility = pd.read_csv('/projects/b1045/Safegraph_v2/mobility_data/2018-01/'+fname+'-part10.csv')
+#naics_list = [722511, 512131]
+
+# limiting data for processing  # TODO: redo the preprocessing into HDF5 format
+# df_mobility = pd.read_csv('/projects/b1045/Safegraph_v2/mobility_data/monthly_combined_files/'+fname+'_compiled.csv')
+df_mobility = pd.read_csv('/projects/b1045/Safegraph_v2/mobility_data/2018-01/'+fname+'-part1.csv')  # TODO: be explicit about column types for loading
 df_filter = df_mobility.filter(['placekey', 'safegraph_place_id', 'naics_code', 'latitude', 'longitude', 'city', 'state',
         'region', 'date_range_start', 'date_range_end', 'raw_visit_counts', 'raw_visitor_counts', 'visits_by_day',
         'visitor_home_cbgs', 'poi_cbg'])
+#df_test = df_filter.loc[(df_mobility['naics_code'] in naics_list)]                                                                       'naics_code'] == 512131)]  # filtering by relevant NAICS codes (722511 (restaurants) and 512131 (movie theaters) for now)
 df_filter = df_filter.loc[(df_mobility['naics_code'] == 722511) | (df_mobility['naics_code'] == 512131)]  # filtering by relevant NAICS codes (722511 (restaurants) and 512131 (movie theaters) for now)
 del df_mobility
 # df_filter.to_csv('test_data/mobility_data/2018-01_filtered.csv', index = False)
@@ -40,37 +43,44 @@ df = pd.read_csv('/projects/b1045/Safegraph_v2/county_data/county_devices_residi
 df_county_sample_size = df.filter(['county', 'county_population', fname])
 del df
 
+# loading in cbg-month factors
+df = pd.read_csv('/projects/b1045/Safegraph_v2/home_panel_summary/cbg_factors_by_month.csv')
+df_cbg_month_factors = df.filter(['cbg', fname])
+del df
 
-def get_daily_true_visits(i):
-    visitor_home_cbgs, raw_visit_count, raw_visitor_count, visits_by_day, poi_cbg, poi_county = get_poi_month_data(i)
+c = 0
+n = 0
+
+def get_daily_true_visits(row):
+    visitor_home_cbgs, raw_visit_count, raw_visitor_count, visits_by_day, poi_cbg, poi_county = get_poi_month_data(row)
     if type(visits_by_day) != np.ndarray or poi_county == '-99999':
         true_visits_by_day = np.nan
         return true_visits_by_day
-    poi_county_pop, poi_county_devices_residing = get_poi_county_data(poi_county, df_county_sample_size)
+    poi_county_pop, poi_county_devices_residing = get_poi_county_data(poi_county, df_county_sample_size)  # TODO: do I need to call this every time?
     cbg_visits_adjusted, cbg_weight = calc_visitor_cbg(visitor_home_cbgs, df_cbg_sample_size, df_pop)
-    remaining_visitors, remaining_adjusted = calc_visitor_poi_home_county(raw_visitor_count, cbg_weight, poi_county_devices_residing, poi_county_pop)
+    remaining_visitors, remaining_adjusted = calc_visitor_poi_home_county(raw_visitor_count, cbg_weight, poi_county_devices_residing, poi_county_pop, c, n)  # TODO: do I need to run this every time?
     cbg_visits_adjusted.append(remaining_adjusted)
     cbg_visits_factor = np.sum(cbg_visits_adjusted) / raw_visitor_count
     true_visits_by_day = visits_by_day * cbg_visits_factor
     return true_visits_by_day
 
 
-def get_poi_month_data(i):
-    visitor_home_cbgs = df_filter.iloc[i]["visitor_home_cbgs"]
-    raw_visit_count = df_filter.iloc[i]["raw_visit_counts"]
-    raw_visitor_count = df_filter.iloc[i]["raw_visitor_counts"]
-    visits_by_day_str = df_filter.iloc[i]["visits_by_day"]
-    if type(visits_by_day_str) != str:
+def get_poi_month_data(row):
+    visitor_home_cbgs = row["visitor_home_cbgs"]  # TODO (DONE): pull row once instead of each time through
+    raw_visit_count = row["raw_visit_counts"]
+    raw_visitor_count = row["raw_visitor_counts"]
+    visits_by_day_str = row["visits_by_day"]
+    if type(visits_by_day_str) != str:  # TODO: is this the best way to check for errors?
         visits_by_day = np.nan
         poi_cbg = np.nan
         poi_county = np.nan
     else:
         visits_by_day = np.array(visits_by_day_str[1:-1].split(',')).astype(int)
-        if np.isnan(df_filter.iloc[i]["poi_cbg"]):
+        if np.isnan(row["poi_cbg"]):
             poi_cbg = '-999999999999'
             poi_county = '-99999'
         else:
-            poi_cbg = str(int(df_filter.iloc[i]["poi_cbg"]))
+            poi_cbg = str(int(row["poi_cbg"]))
         if len(poi_cbg) < 12:
             poi_county = '0' + poi_cbg[0:4]
         elif len(poi_cbg) == 12:
@@ -85,18 +95,18 @@ def get_poi_county_data(poi_county, county_pops):
     return poi_county_pop.iloc[0], poi_county_devices_residing.iloc[0]
 
 
-def calc_visitor_cbg(visitor_home_cbgs, cbg_sample_size_df, cbg_pop_df):
-    cbg_visits_adjusted = []
-    cbg_weight = []
+def calc_visitor_cbg(visitor_home_cbgs, cbg_sample_size_df, cbg_pop_df):  # TODO: should I pass these datasets in here or is this just a waste of time?
+    cbg_visits_adjusted = []  # TODO: can I preallocate these based on visitor_home_cbgs.split()?
+    cbg_weight = []  # TODO: can I use dicts instead of lists?
     if visitor_home_cbgs == '{}':
         return cbg_visits_adjusted, cbg_weight
     for visitor_home_cbg_str in visitor_home_cbgs[1:-1].split(','):
-        visitor_home_cbg_num = np.int(visitor_home_cbg_str[1:13])
+        visitor_home_cbg_num = np.int(visitor_home_cbg_str[1:13])  # TODO: is there a more efficient way to handle object type?
         raw_visit_home_cbg = np.int(visitor_home_cbg_str[-1])
         if raw_visit_home_cbg < 5:
             raw_visit_home_cbg = 3  # assuming 3 if raw_visits is less than 4
-        cbg_weight.append(raw_visit_home_cbg)
-        visits_adjusted = get_cbg_visits_adjusted(visitor_home_cbg_num, raw_visit_home_cbg, cbg_sample_size_df, cbg_pop_df)
+        cbg_weight.append(raw_visit_home_cbg)  # TODO: move this further down to avoid the double calculation
+        visits_adjusted = get_cbg_visits_adjusted(visitor_home_cbg_num, raw_visit_home_cbg)
         cbg_visits_adjusted.append(visits_adjusted)
         if visits_adjusted == -999:
             del cbg_weight[-1]
@@ -104,18 +114,23 @@ def calc_visitor_cbg(visitor_home_cbgs, cbg_sample_size_df, cbg_pop_df):
     return cbg_visits_adjusted, cbg_weight
 
 
-def get_cbg_visits_adjusted(home_cbg_num, raw_visits, cbg_sample_size_df, cbg_pop_df):
-    cbg_sample_size = cbg_sample_size_df.loc[(cbg_sample_size_df['census_block_group'] == home_cbg_num)]['number_devices_residing']
-    cbg_pop = cbg_pop_df.loc[(cbg_pop_df['census_block_group'] == home_cbg_num)]['B01001e1']
-    if cbg_pop.empty or cbg_sample_size.empty:
+def get_cbg_visits_adjusted(home_cbg_num, raw_visits):
+    cbg_factor = df_cbg_month_factors.loc[(df_cbg_month_factors['cbg'] == home_cbg_num)][fname]  # TODO: is this the fastest way to perform this lookup?
+    if np.size(cbg_factor) == 0:
         return -999
-    home_cbg_visits_adjusted = raw_visits / cbg_sample_size.iloc[0] * cbg_pop.iloc[0]
-    return home_cbg_visits_adjusted
+    cbg_factor = cbg_factor.iloc[0]
+    if cbg_factor <= 0:
+        return -999
+    return raw_visits * cbg_factor
 
 
-def calc_visitor_poi_home_county(raw_visitor_count, cbg_weight, poi_county_devices_residing, poi_county_pop):
+def calc_visitor_poi_home_county(raw_visitor_count, cbg_weight, poi_county_devices_residing, poi_county_pop, c, n):
     remaining_visitors = np.int(raw_visitor_count - sum(cbg_weight))
     remaining_adjusted = remaining_visitors / poi_county_devices_residing * poi_county_pop
+    if remaining_adjusted == 0:
+        c += 1
+    elif remaining_adjusted < 0:
+        n += 1
     return remaining_visitors, remaining_adjusted
 
 
@@ -129,23 +144,26 @@ longitude = np.ndarray(len_ind); longitude[:] = np.nan
 city = np.ndarray(len_ind, dtype='object'); city[:] = np.nan
 state = np.ndarray(len_ind, dtype='object'); state[:] = np.nan
 true_visits_by_day = np.ndarray(shape=(len_ind, 31)); true_visits_by_day[:] = np.nan
+df_filter = df_filter.reset_index()
 
 t = time.time()
-for index in np.arange(len_ind):
-#for index in np.arange(26884, len_ind):
+for index, row in df_filter.iterrows():
+# for index in np.arange(len_ind):  # TODO (DONE): update this to iterrows
     if index % 1000 == 0:
         print(np.round(index/len_ind*100, 2))
         print(time.time() - t)
-    placekey[index] = df_filter.iloc[index]['placekey']
-    naics_code[index] = df_filter.iloc[index]['naics_code']
-    latitude[index] = df_filter.iloc[index]['latitude']
-    longitude[index] = df_filter.iloc[index]['longitude']
-    city[index] = df_filter.iloc[index]['city']
-    state[index] = df_filter.iloc[index]['region']
+        print(index)
+        print(row)
+    placekey[index] = row['placekey']
+    naics_code[index] = row['naics_code']
+    latitude[index] = row['latitude']
+    longitude[index] = row['longitude']
+    city[index] = row['city']
+    state[index] = row['region']
     if state[index] in ['VI', 'AS', 'GU']:
         true_visits_by_day[index] = np.nan
         continue
-    true_visits_by_day[index] = np.round(get_daily_true_visits(index), 2)
+    true_visits_by_day[index] = np.round(get_daily_true_visits(row), 2)
 
 poi_mon_data['placekey'] = pd.Series(placekey)
 poi_mon_data['naics_code'] = pd.Series(naics_code)
